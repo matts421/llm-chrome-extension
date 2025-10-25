035edc1731d8d68f30898f3faf82d157daaed2c4
Matthew <matthewasmith442@gmail.com>
2025-10-25 14:06:22 -0700

    fix

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ load_dotenv()
 SERVICE_ACCOUNT_INFO = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SHEET_ID = os.environ.get("SHEET_ID")
-RANGE_NAME = "Sheet1!A:C"
+RANGE_NAME = "Sheet1!A:D"
 
 credentials = service_account.Credentials.from_service_account_info(
     SERVICE_ACCOUNT_INFO, scopes=SCOPES
